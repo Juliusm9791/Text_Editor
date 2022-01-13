243795defa63925f056dba07fa195324156a68d2
Julius Markauskas <juliusm9791@gmail.com>
2022-01-13 12:04:41 -0500

    cleaned code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-// const { GenerateSW } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -29,7 +26,7 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      // new GenerateSW(),
+
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
